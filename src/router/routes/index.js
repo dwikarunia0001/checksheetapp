06212247c7d027c56bea648c0d6555d0db12d4ca
dwikarunia0001 <dwikarunia14157375@gmail.com>
2024-07-28 21:24:24 +0700

    3 dashboard feature fix

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,7 +1,6 @@
 import auth from './auth';
 import dashboard from './dashboard';
 import checksheet from './checksheet';
-import report from './report';
 import admdashboard from './admdashboard';
 import checkpoint from './checkpoint';
 import recentchecksheet from './recentchecksheet';
@@ -15,7 +14,6 @@ const routes = [
   dashboard,
   auth,
   checksheet,
-  report,
   admdashboard,
   checkpoint,
   recentchecksheet,
